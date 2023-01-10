d9fc8e5ba17459c9250fe5e7440bc8861dde4837
Your Name <elizabeth.ramos101@hotmail.com>
2023-01-10 12:39:08 +0000

    h

--- src/controllers/music.controllers.js
+++ src/controllers/music.controllers.js
@@ -1,24 +1,53 @@
 const allAlbums = async (req, res, next) => { //req=loque se manda, res= la respuesta
     //console.log('all albums');
-    try{
-        res.status(200).send({ status: 'OK', msg: "All Albums"})
+    try {
+        res.status(200).send({ status: 'OK', msg: "All Albums" })
         //next()
     } catch (error) {
-        req.status(500).send({status:'FALSE'})
+        req.status(500).send({ status: 'FALSE' })
         //next()
     }
     //next()
+}
+
+const singleAlbum = async (req, res, next) => {
+    const { id } = req.params
+    try {
+        res.status(200).send({ status: 'OK', msg: `Get album with ID ${id}` })
+    } catch (error) {
+        res.status(500).send({ status: 'FALSE' })
+    }
+}
+
+const deleteAlbum = async (req, res, next) => {
+    const { id } = req.params
+    try {
+        res.status(200).send({ status: 'OK', msg: `Deleted album with id ${id}` })
+    } catch (error) {
+        res.status(500).send({ status: 'FALSE' })
+    }
+}
+
+const updateAlbum = async (req, res, next) => {
+    const { id } = req.params
+    const body = req.body
+
+    try {
+        res.status(200).send({ status: 'OK', msg: `Updated album with id ${id}`, body })
+    } catch (error) {
+        res.status(500).send({ status: 'FALSE' })
+    }
 }
 
 const createAlbum = async (req, res, next) => {
-    const {id} = req.params
+    //const {id} = req.params
     const body = req.body
-    console.log(body);
-    try{
-        res.status(201).send({ status: 'OK', msg: `Created album with ${id}`})
+    //console.log(body);
+    try {
+        res.status(201).send({ status: 'OK', msg: `Created album with ${id}` })
         //next()
     } catch (error) {
-        req.status(500).send({status:'FALSE'})
+        req.status(500).send({ status: 'FALSE' })
         //next()
     }
     //next()
@@ -26,4 +55,4 @@ const createAlbum = async (req, res, next) => {
 
 
 
-module.exports = {allAlbums, createAlbum}
+module.exports = { allAlbums, updateAlbum, deleteAlbum, singleAlbum, createAlbum }
