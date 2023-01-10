8c36146ff3d51046aef5302282e7307505f72452
Your Name <elizabeth.ramos101@hotmail.com>
2023-01-10 14:37:44 +0000

    package

---
+++ src/config/config.js
@@ -0,0 +1,45 @@
+const dotenv = require("dotenv");
+const logger = require("loglevel");
+
+dotenv.config();
+
+const ENV = process.env.NODE_ENV || 'development'
+
+logger.enableAll();
+
+const CONFIG = {
+  development: {
+    app: {
+      PORT: process.env.PORT || 4002,
+    },
+    logger: {
+      warn: logger.warn,
+      info: logger.info,
+      error: logger.error,
+      trace: logger.trace,
+      debug: logger.debug,
+    },
+    //conectamos nuestra URI a la Base de datos
+    db: {
+      uri: process.env.MONGODB_URI_CLUSTER,
+    }, //nuestro archivo de.json ya no  nos hace falta 
+    //creamos el archivo connect.js
+  },
+  production: {
+    app: {
+      PORT: process.env.PORT || 4002,
+    },
+    logger: {
+      warn: logger.warn,
+      info: logger.info,
+      error: logger.error,
+      trace: logger.trace,
+      debug: logger.debug,
+    },
+    db: {
+      url: "http://localhost:4002/albums",
+    },
+  },
+};
+
+module.exports = CONFIG[ENV];
